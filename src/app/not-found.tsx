db6f3d0383c02b9a006d7aca55933cf3c4ee4905
nabilWafick <nabilsoumanou7@gmail.com>
2025-02-07 07:17:56 +0100

    fix issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,12 +7,14 @@ export default function NotFound() {
         <h2 className="text-2xl md:text-3xl font-semibold text-secondary mb-4">
           404 - Page Non Trouvé
         </h2>
-        <p className="text-foreground mb-8">La page recherchée n'existe pas</p>
+        <p className="text-foreground mb-8">
+          La page recherchée n&apos;existe pas
+        </p>
         <Link
           href="/"
           className="px-6 py-3 rounded border border-secondary text-secondary hover:bg-secondary/10 transition-colors inline-block"
         >
-          Page d'accueil
+          Page d&apos;accueil
         </Link>
       </div>
     </div>
